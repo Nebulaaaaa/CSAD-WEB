836574e644dc60b7598c37998ece84946bd7d61f
Kzeezee <68066041+Kzeezee@users.noreply.github.com>
2020-07-30 13:33:03 +0800

    Create class3.php

---
+++ CSADWeb/class3.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylemain.css">
+    <title>Internet Fundamentals</title>
+</head>
+<body>
+<header class="header">
+    <nav class="nav_bar">
+      <div class="logo">
+        <a href="index.html"><img style="width: 60px; height: 60px;" src="img/logo.png" alt="logo" /></a>
+      </div>
+      <ul class="nav-links">
+        <li><a href="" class="nav-link">Classes</a></li>
+        <li><a href="" class="nav-link">FAQs</a></li>
+        <li><a href="" class="nav-link">Contact Us</a></li>
+        <li><a href="" class="nav-link">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <div class="class-upper-container">
+        <div class="class-title">Internet Fundamentals</div>
+        <div class="class-catchphrase">Surf the web like a pro</div>
+        <div class="ratings-container">
+            <div class="ratings-stars">
+                <span class="rating-star checked"></span>
+                <span class="rating-star checked"></span>
+                <span class="rating-star checked"></span>
+                <span class="rating-star checked"></span>
+                <span class="rating-star"></span>
+            </div>
+            <div class="ratings-text">
+                <span class="ratings-decimal">4.0/5.0</span>
+                <span class="ratings-total">(253 ratings)</span>
+            </div>
+        </div>
+    </div>
+    <div class="class-lower-container">
+        <div class="class-desc-box">
+            <div class="desc-title">
+                What you will learn:
+            </div>
+            <p class="syllabus-left">
+                - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque felis imperdiet proin fermentum. Ridiculus mus mauris vitae ultricies. Augue eget arcu dictum varius duis at consectetur lorem. Pretium lectus quam id leo in vitae turpis.
+            </p>
+            <p class="syllabus-right">
+                - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque felis imperdiet proin fermentum. Ridiculus mus mauris vitae ultricies. Augue eget arcu dictum varius duis at consectetur lorem. Pretium lectus quam id leo in vitae turpis.
+            </p>
+        </div>
+        <div class="button-container">
+            <input type="button" class="enroll-now-3" value="Enroll Now">
+        </div>
+    </div>
+    <div class="class-card">
+        <div class="img-gallery">
+            <img src="" alt="InternetFundamentals" class="class-3">
+        </div>
+        <div class="class-card-text-container">
+            <div class="class-card-text">
+                <div class="price">$50</div>
+                <div class="includes">Includes</div>
+                <span class="class-package">
+                    Weekly 2 hours lesson on available days
+                    Pre-paid Learning Materials
+                    Certificate of Completion
+                </span>
+            </div>
+        </div>
+    </div>
+  </main>
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="wrapper">
+        <div class="wrapper-container">
+          <div class="column">
+            <h5 class="footer-heading">Services</h5>
+            <ul>
+              <li>
+                <a href="">Classes</a>
+              </li>
+            </ul>
+          </div>
+          <div class="column">
+            <h5 class="footer-heading">Seniors Initialize</h5>
+            <ul>
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div class="column">
+            <h5 class="footer-heading">Community</h5>
+            <ul>
+              <li>
+                <a href="">Forums</a>
+              </li>
+            </ul>
+          </div>
+          <div class="column">
+            <h5 class="footer-heading">Connections</h5>
+            <img src="instagram.svg" alt="instagram" class="instagram-logo">
+            <ul>
+              <li>
+                <a href="">Instagram</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
