2aef4973a4a668a69fd02f91cc2007c746f81601
Nebulaaaaa <66403278+Nebulaaaaa@users.noreply.github.com>
2020-08-10 16:26:19 +0800

    Add files via upload

--- CSADWeb/contactus.php
+++ CSADWeb/contactus.php
@@ -29,15 +29,14 @@
             <section class="upper">
                 <div class="main-content">
                     <div class="middle">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.775876175282!2d103.77536081457261!3d1.3097756990447076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a602ff17c15%3A0xa9545dd23993859e!2sSingapore%20Polytechnic!5e0!3m2!1sen!2ssg!4v1596525263868!5m2!1sen!2ssg" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
                         <h1 class="contactus-text">Contact Us</h1>
                         <p class="upper-text">Physical payment or need<br>a face to face talk?</p>
                         <p class="lower-text">Office:
                             <br>
                             500 Dover Road, Singapore Polytechnic, Singapore 139651
                         </p>
-                        <div class="map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.775876175282!2d103.77536081457261!3d1.3097756990447076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a602ff17c15%3A0xa9545dd23993859e!2sSingapore%20Polytechnic!5e0!3m2!1sen!2ssg!4v1596525263868!5m2!1sen!2ssg" width="400" height="350" frameborder="" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                        </div>
                     </div>
                     <div class="transition-text">
                         Too Troublesome? Email Us.
@@ -55,7 +54,7 @@
                         <textarea name="message-box" id="message" cols="30" rows="10" class="message-box"></textarea>
                     </div>
                     <div class="ending-text">
-                        Email us with any questions or inquiries or call 91234567. We would be happy to answer your questions and set up a meeting with you! Stay webbing!
+                        Email us with any questions or inquiries or call 91234567. We would be happy to answer your<br>questions and set up a meeting with you! Stay webbing!
                     </div>
                 </div>
             </section>
